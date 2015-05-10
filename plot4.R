27dcdcf3b984fec4e53ee442725899b9fbec5c9b
broome <broome.jai@gmail.com>
2015-05-10 19:51:07 -0400

    updated plot4.R

--- plot4.R
+++ plot4.R
@@ -6,8 +6,17 @@ plot(HPC$dateTime, HPC$Global_active_power, type = "l", ylab = "Global Active Po
      xlab = "", xaxt = "n")
 
 plot(HPC$dateTime, HPC$Sub_metering_1, type = "l", ylab = "Energy sub metering",
-     xlab = """) #, xaxt = "n"
+     xlab = "")
 lines(HPC$dateTime, HPC$Sub_metering_2, col = "red", lwd = 1)
 lines(HPC$dateTime, HPC$Sub_metering_3, col = "blue", lwd = 1)
 
+plot(HPC$dateTime, HPC$Voltage, type = "l", ylab = "Voltage",
+     xlab = "datetime")
+
+plot(HPC$dateTime, HPC$Global_reactive_power, type = "l", ylab = "Global_reactive_power",
+     xlab = "datetime")
+
 dev.off()
+
+ #, xaxt = "n"
+
